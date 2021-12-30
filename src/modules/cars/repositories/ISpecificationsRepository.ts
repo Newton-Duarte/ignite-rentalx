d99fa287a5ae66f1716cd99aa079696ca699a3f3
Newton Duarte <newton_@hotmail.com>
2021-12-30 10:40:49 -0300

    feat: add promise return type for interfaces

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../model/Specification';
+import { Specification } from '../entities/Specification';
 
 interface ICreateSpecificationDTO {
   name: string;
@@ -6,9 +6,9 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationsRepository {
-  create({ name, description }: ICreateSpecificationDTO): Specification;
-  list(): Specification[];
-  findByName(name: string): Specification;
+  create({ name, description }: ICreateSpecificationDTO): Promise<Specification>;
+  list(): Promise<Specification[]>;
+  findByName(name: string): Promise<Specification>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
