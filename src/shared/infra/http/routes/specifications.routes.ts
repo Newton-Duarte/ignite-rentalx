5cbc5a39525fd4abec757793d5335de63620ce20
Newton Duarte <newton_@hotmail.com>
2022-01-29 12:51:03 -0300

    feat: add ensure authenticated and ensure admin to especifications routes

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,12 +3,15 @@ import { Router } from 'express';
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListSpecificationsController } from '@modules/cars/useCases/listSpecifications/ListSpecificationsController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
 specificationsRoutes.get('/', listSpecificationsController.handle);
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post('/', ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 export { specificationsRoutes };
