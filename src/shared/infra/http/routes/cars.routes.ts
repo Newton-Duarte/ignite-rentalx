9879249689740a7e24d245a9bb0d9d5f86469d0b
Newton Duarte <newton_@hotmail.com>
2022-01-29 15:19:08 -0300

    feat: add create car specification route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
@@ -10,8 +11,10 @@ const carsRoutes = Router();
 
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarController = new CreateCarController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.get('/available', listAvailableCarsController.handle);
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
+carsRoutes.post('/:id/specifications', ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle);
 
 export { carsRoutes };
