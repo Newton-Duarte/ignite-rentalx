2f2392c74959df563daa2c15b80f7091fdcfb461
Newton Duarte <newton_@hotmail.com>
2022-01-29 13:58:14 -0300

    feat: add find all available function to cars repositories

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -36,11 +36,33 @@ class CarsRepository implements ICarsRepository {
     return car;
   }
   async findByLicensePlate(license_plate: string): Promise<Car> {
-    const car = this.repository.findOne({
+    const car = await this.repository.findOne({
       license_plate,
     });
 
     return car;
+  }
+
+  async findAllAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
+    const carsQuery = await this.repository
+      .createQueryBuilder('cars')
+      .where('cars.available = :available', { available: true });
+
+    if (brand) {
+      carsQuery.andWhere('cars.brand = :brand', { brand });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere('cars.category_id = :category_id', { category_id });
+    }
+
+    if (name) {
+      carsQuery.andWhere('cars.name = :name', { name });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
   }
 }
 
