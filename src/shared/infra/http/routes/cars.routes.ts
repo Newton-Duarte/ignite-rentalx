c45d10b6fd49a96bf35a5b33d96e1f11c6048174
Newton Duarte <newton_@hotmail.com>
2022-01-29 13:58:54 -0300

    feat: add get available cars route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
+const listAvailableCarsController = new ListAvailableCarsController();
 const createCarController = new CreateCarController();
 
+carsRoutes.get('/available', listAvailableCarsController.handle);
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 export { carsRoutes };
